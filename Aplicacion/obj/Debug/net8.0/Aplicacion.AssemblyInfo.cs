e7c008d7f4f530a39176843d53b41ebb1b5d3ffa
MrFahrenheit720 <felipeg9906@hotmail.com>
2025-05-22 16:24:51 -0500

    Terminado

--- Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
+++ Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3630ca17b70f7529f40c8579f081e3333d65c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76ede9e1ea952f5c4ce087a316088da556d7fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
